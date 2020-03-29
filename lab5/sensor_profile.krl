e18ca3a65f84bef84b1bed3cebe30aec7e2820fb
Gaven Finch <gavenfinch@gmail.com>
2020-03-28 21:58:15 -0600

    Finish profile page

--- lab5/sensor_profile.krl
+++ lab5/sensor_profile.krl
@@ -7,8 +7,8 @@ ruleset sensor_profile {
     global {
         get_profile_data = function() {
             {
-                "location": ent:location,
                 "name": ent:name,
+                "location": ent:location,
                 "SMS_number": ent:SMS_number,
                 "threshold": ent:threshold
             }
@@ -18,8 +18,8 @@ ruleset sensor_profile {
     rule initialization {
         select when wrangler ruleset_added where event:attr("rids") >< meta:rid
         fired{
-            ent:location := "default location"
             ent:name := "default name"
+            ent:location := "default location"
             ent:SMS_number := "+18017934946"
             ent:threshold := 80
         }
@@ -28,11 +28,16 @@ ruleset sensor_profile {
     rule update {
         select when sensor profile_updated
         pre {
-            eventattrs = event:attrs
-            SMS_number = eventattrs{"SMS_number"} => "+" + eventattrs{"SMS_number"} | ent:SMS_number
-            threshold = eventattrs{"threshold"} => eventattrs{"threshold"}.as("Number") | ent:threshold
+            eventattrs = event:attrs.klog("event attributes: ")
+            decoded = eventattrs.decode()
+            name = decoded{"name"} => decoded{"name"} | ent:name
+            location = decoded{"location"} => decoded{"location"} | ent:location
+            SMS_number = decoded{"SMS_number"} => "+" + decoded{"SMS_number"} | ent:SMS_number
+            threshold = decoded{"threshold"} => decoded{"threshold"}.as("Number") | ent:threshold
         }
         fired {
+            ent:name := name
+            ent:location := location
             ent:SMS_number := SMS_number
             ent:threshold := threshold
         }
--- lab5/spa/src/Profile.js
+++ lab5/spa/src/Profile.js
@@ -1,10 +1,92 @@
 import React from 'react';
+import './Profile.css';
+
+import axios from 'axios';
 
 class Profile extends React.Component {
+    state = {
+        profile: null,
+        new_name: "",
+        new_location: "",
+        new_SMS_number: "",
+        new_threshold: ""
+
+    }
+
+    async componentDidMount() {
+        const URL = "http://localhost:8080/sky/cloud/K5P6PzpMBd7bU7Z3ijAcao/sensor_profile/get_profile_data";
+        const response = await axios.get(URL);
+        const data = await response.data;
+        this.setState({profile: data})
+        console.log(data);
+    }
+
+    handleClick = async () => {
+        const URL = "http://localhost:8080/sky/event/K5P6PzpMBd7bU7Z3ijAcao/1/sensor/profile_updated";
+        const obj = {
+            name: this.state.new_name,
+            location: this.state.new_location,
+            SMS_number: this.state.new_SMS_number,
+            threshold: this.state.new_threshold,
+        }
+        const response = await axios.post(URL,obj);
+        window.location.reload(false);
+    }
+
+    handleChange = (e) => {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
     render() {
         return (
-            <div>
-                This will be the profile page.
+            this.state.profile === null
+            ?
+            <div>Loading...</div>
+            :
+            <div className="Profile">
+                <h1>Current Profile Information</h1>
+
+                <div>
+                    <b>Name:</b> {this.state.profile.name}
+                    <input
+                        type="text"
+                        placeholder="Enter a new name?"
+                        value={this.state.new_name}
+                        name="new_name"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    <b>Location:</b> {this.state.profile.location}
+                    <input
+                        type="text"
+                        placeholder="Enter a new location?"
+                        value={this.state.new_location}
+                        name="new_location"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    <b>Phone Number to Notify:</b> {this.state.profile.SMS_number.slice(1)}
+                    <input
+                        type="text"
+                        placeholder="Enter a new number?"
+                        value={this.state.new_SMS_number}
+                        name="new_SMS_number"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    <b>Temperature Violation Threshold:</b> {this.state.profile.threshold}
+                    <input
+                        type="text"
+                        placeholder="Enter a new threshold?"
+                        value={this.state.new_threshold}
+                        name="new_threshold"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <button onClick={this.handleClick}>Submit changes</button>
             </div>
         )
     }
